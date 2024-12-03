2663428571e727564a8f6a11dce0ea4e8189a290
pica69co <oscarwarrieta@gmail.com>
2024-12-03 15:50:47 -0500

    Enhance Navbar and Footer components with theme toggle and social media links

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,7 +1,42 @@
 import React from "react";
+import Image from "next/image";
+import { useTheme } from "next-themes";
+
+import images from "../assets";
+import { Button } from ".";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  const { theme, setTheme } = useTheme();
+
+  return (
+    <footer className="flexCenter flex-col border-t dark:border-nft-black-1 border-nft-gray-1 sm:py-8 py-16">
+      <div className="flexCenter w-full mt-5 border-t dark:border-nft-black-1 border-nft-gray-1 sm:px-4 px-16">
+        <div className="flexBetween flex-row w-full minmd:w-4/5 sm:flex-col mt-7">
+          <p className="font-poppins dark:text-white text-nft-black-1 font-semibold text-base">
+            {" "}
+            &copy; OswarKet, Inc.
+          </p>
+          {[
+            images.instagram,
+            images.twitter,
+            images.telegram,
+            images.discord,
+          ].map((image, idx) => {
+            <div key={idx} className="mx-2 cursor-pointer">
+              <Image
+                src={image}
+                objectFit="contain"
+                width={32}
+                height={32}
+                alt="logo"
+                className={theme === "light" && "filter invert"}
+              />
+            </div>;
+          })}
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -5,10 +5,50 @@ import Image from "next/image";
 import Link from "next/link";
 
 import images from "../assets";
-import { id } from "../node_modules/next/dist/compiled/webpack/bundle5";
+
+const MenuItems = ({ isMobile, active, setActive }) => {
+  const generateLink = (idx) => {
+    switch (idx) {
+      case 0:
+        return "/";
+      case 1:
+        return "/created-nft";
+      case 2:
+        return "/my-nfts";
+      default:
+        return "/";
+    }
+  };
+
+  return (
+    <ul
+      className={`list-none flexCenter flex-row ${
+        isMobile && `flex-col h-full`
+      }`}
+    >
+      {["Explore NFTs", "Listed NFTs", "My NFTs"].map((item, idx) => (
+        <li
+          key={idx}
+          onClick={() => {
+            setActive(item);
+          }}
+          className={`flex flex-row items-center font-poppins font-semibold text-base dark:hover:text-white hover:text-nft-dark mx-3 ${
+            active === item
+              ? "dark:text-white text-nft-black-1"
+              : "dark:text-nft-gray-3 text-nft-gray-2"
+          }`}
+        >
+          <Link href={generateLink(idx)}>{item}</Link>
+        </li>
+      ))}
+    </ul>
+  );
+};
 
 const Navbar = () => {
   const { theme, setTheme } = useTheme();
+  const [active, setActive] = useState("Explore NFTs");
+
   // console.log({ theme });
 
   return (
@@ -47,13 +87,21 @@ const Navbar = () => {
             type="checkbox"
             id="checkbox"
             className="checkbox"
-            onChange={() => {}}
+            onChange={() => setTheme(theme === "light" ? "dark" : "light")}
           />
           <label
             htmlFor="checkbox"
             className="flexBetween w-8 h-4 bg-black rounded-2xl p-1 relative label"
-          ></label>
+          >
+            <i className="fas fa-sun" />
+            <i className="fas fa-moon" />
+            <div className="w-3 h-3 absolute bg-white rounded-full ball" />
+          </label>
         </div>
+      </div>
+      <div>
+        {" "}
+        <MenuItems active={active} setActive={setActive} />{" "}
       </div>
     </nav>
   );
