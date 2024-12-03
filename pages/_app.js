b63c6d48459e25ff5525efae220aafc4e8a5a5cf
pica69co <oscarwarrieta@gmail.com>
2024-12-02 20:11:44 -0500

    Add Navbar and Footer components with layout integration

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,23 @@
-import '../styles/globals.css'
+import { ThemeProvider } from "next-themes";
+import Script from "next/script";
+
+import "../styles/globals.css";
+import { Navbar, Footer } from "../components";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider attribute="class">
+      <div className="dark:bg-nft-dark bg-white min-h-screen">
+        <Navbar />
+        <Component {...pageProps} />
+        <Footer />
+      </div>
+      <Script
+        src="https://kit.fontawesome.com/adfc1cab73.js"
+        crossorigin="anonymous"
+      />
+    </ThemeProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
