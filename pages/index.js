735a3d0dc69ae3b09c4b9cdacd58f269244e32b9
pica69co <oscarwarrieta@gmail.com>
2024-12-03 21:49:53 -0500

    Add CreatorCard  implement makeId utility for unique identifier generation

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,13 @@
-import Banner from "../components/Banner";
+import { useEffect, useRef, useState } from "react";
+import { Banner, CreatorCard } from "../components";
+import images from "../assets";
+import { makeId } from "../utils/makeId";
 
-export default function Home() {
+const Home = () => {
+  const parentRef = useRef();
+  const scrollRef = useRef();
+  // console.log(makeId(3));
+
   return (
     <div className="flex justify-center sm:px-4 p-12">
       <div className="w-full minmd:w-4/5">
@@ -11,7 +18,31 @@ export default function Home() {
           }
           childStyles={"md:text-4xl sm:text-2xl xs:text-xl text-left"}
         />
+        <div>
+          <h1 className="font-poppins dark:text-white text-nft-black-1 text-2xl minlg:text-4xl font-semibold ml-4 xs:ml-0">
+            Best Creators
+          </h1>
+          <div className="relative flex-1 max-w-full flex mt-3" ref={parentRef}>
+            <div
+              className="flex flex-row w-max overflow-x-scroll no-scrollbar select-none"
+              ref={scrollRef}
+            >
+              {[6, 7, 8, 9, 10].map((item, index) => (
+                <CreatorCard
+                  key={`creator-${index}`}
+                  item={item}
+                  rank={index}
+                  creatorImage={images[`creator${index}`]}
+                  creatorName={`0x${makeId(3)}...${makeId(4)}`}
+                  creatorEths={10 - index * 0.5}
+                />
+              ))}
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
-}
+};
+
+export default Home;
